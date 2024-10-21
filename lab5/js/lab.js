24a278f9976c37a45c56d98a988738b7ae772479
tsmall1123 <tylersmall1123@gmail.com>
2024-10-21 16:49:56 -0700

    Lab 5
    Lab 5 completed and submitted with reflection

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,19 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Matan Maoz
+// Date: 10.21.24
 
-// Constants
+// Defined Variables
+
+var make = "Mercedes";
+var model = "G-Wagon";
+var color = "Black";
+var year = 2020;
+
 
 // Functions
+// Calculate age
+age = 2024 - year;
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
@@ -12,6 +21,7 @@ function myFunction(param1, param2) {
   // return results;
 }
 
+// Main function
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
@@ -19,3 +29,9 @@ function main() {
 
 // let's get this party started
 main();
+// Output to the document
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
